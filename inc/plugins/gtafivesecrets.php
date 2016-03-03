91af6169c55e099a052e1088dbe91a34c7c42e28
Mark Topper <mail@webman.io>
2016-03-03 20:51:12 +0100

    Update domain name
    Change domain from `gta5secrets.net` to `gta5secrets.com`.

--- inc/plugins/gtafivesecrets.php
+++ inc/plugins/gtafivesecrets.php
@@ -2,7 +2,7 @@
 /**
  * GTA 5 Secrets Map BBCode
  * Author: Mark Topper <mail@webman.io>
- * Version 1.0
+ * Version 1.1
  */
 
 
@@ -10,11 +10,11 @@ $plugins->add_hook("parse_message", "gtafivesecrets_run");
 function gtafivesecrets_info() {
 	return array(
 		"name"			=> "GTA 5 Secrets Map BBCode",
-		"description"	=> "This plugin will enable a BBCode for showing GTA 5 Secrets Map just like on www.gta5secrets.net. This supports the MyCode output from the websites Sharing Dialog.",
-		"website"		=> "http://www.gta5secrets.net/",
+		"description"	=> "This plugin will enable a BBCode for showing GTA 5 Secrets Map just like on gta5secrets.com. This supports the MyCode output from the websites Sharing Dialog.",
+		"website"		=> "http://gta5secrets.com/",
 		"author"		=> "Mark Topper",
 		"authorsite"	=> "http://www.webman.io/",
-		"version"		=> "1.0",
+		"version"		=> "1.1",
 		"guid"			=> "f25be3f3277cc1b7629a1193a1e35aca"
 	);
 }
@@ -37,7 +37,7 @@ function gtafivesecrets_run($message) {
 
 	$mycode = array(
 	        //Text Apperence
-		'#\[map\]http\://(www\.)?gta5secrets.net(.*?)\[/map\]#si' => '<iframe src="http://www.gta5secrets.net\\2" height="500" style="width: 100%;">Your browser doesn\'t support iFrames.</iframe>'
+		'#\[map\]http\://(www\.)?gta5secrets.com(.*?)\[/map\]#si' => '<iframe src="http://www.gta5secrets.com\\2" height="500" style="width: 100%;">Your browser doesn\'t support iFrames.</iframe>'
 	);
 
 	$message = preg_replace(array_keys($mycode), array_values($mycode), $message);
